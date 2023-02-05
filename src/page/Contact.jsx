89db37d7d053f5d80fba8d05a1d1790aee34730f
Zeddnyx <96564938+Zeddnyx@users.noreply.github.com>
2023-02-05 18:56:50 +0700

    Update Contact.jsx

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -6,9 +6,9 @@ import { AiFillHeart } from 'react-icons/ai'
 import { BsGithub, BsTelegram, BsInstagram, BsLinkedin } from 'react-icons/bs'
 
 export default function Contact() {
-  const [email, setlEmail] = useState('')
-  const [name, setlName] = useState('')
-  const [msg, setlMsg] = useState('')
+  const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
+  const [msg, setMsg] = useState('')
 
   const [notif, setNotif] = useState(false)
   const handleNotif = () => {
@@ -47,8 +47,8 @@ export default function Contact() {
       </div>
 
       <form onSubmit={handleSub} className={form}>
-        <input className={input} required onChange={e => setlEmail(e.target.value)} type="email"  placeholder='example@blabla.com'/>
-        <input className={input} required onChange={e => setlName(e.target.value)} type="text"  placeholder='name'/>
+        <input className={input} required onChange={e => setEmail(e.target.value)} type="email"  placeholder='example@blabla.com'/>
+        <input className={input} required onChange={e => setName(e.target.value)} type="text"  placeholder='name'/>
         <textarea className={textarea} required onChange={e => setlMsg(e.target.value)} placeholder='type something...'> </textarea>
         <button className={btn} type="submit" onClick={handleNotif}><span className={iconSend}><IoIosSend size='25' /></span></button>
       </form>
@@ -60,14 +60,14 @@ export default function Contact() {
     <a className={aSosmed} href="https://github.com/Zeddnyx"><BsGithub size='20' /></a>
     <a className={aSosmed} href="https://t.me/Zeddnyx"><BsTelegram size='20'/></a>
     <a className={aSosmed} href="https://instagram.com/zedd.nyx"><BsInstagram size='20'/></a>
-    <a className={aSosmed} href="https://linkdn.com/Jhondoe"><BsLinkedin size='20'/></a>
+    <a className={aSosmed} href="https://www.linkedin.com/in/zedd-j-650482199"><BsLinkedin size='20'/></a>
   </div>
 
   <div className={divSosmedDesk}>
     <a className={aSosmed} href="https://github.com/Zeddnyx"><BsGithub size='25' /></a>
     <a className={aSosmed} href="https://t.me/Zeddnyx"><BsTelegram size='25' /></a>
     <a className={aSosmed} href="https://instagram.com/zedd.nyx"><BsInstagram size='25' /></a>
-    <a className={aSosmed} href="https://linkdincom/Jhondoe"><BsLinkedin size='25' /></a>
+    <a className={aSosmed} href="https://www.linkedin.com/in/zedd-j-650482199"><BsLinkedin size='25' /></a>
     <div className={line}></div>
   </div>
     
@@ -81,7 +81,7 @@ const h1 = 'text-2xl sm:text-4xl text-gray1 md:text-5xl font-bold font-ls hover:
 const p = 'p-5 text-gray3 font-semibold text-sm lg:text-md md:px-10 lg:px-40'
 
 const form = 'grid grid-cols-1 gap-4 max-w-xs mx-auto'
-const input = 'bg-transparent text-gray1 hover:bg-bg2 lg:w-96 outline-none rounded border-2 border-cyan p-2 text-gray1 text-sm lg:text-md'
+const input = 'bg-transparent text-gray1 hover:bg-bg2 lg:w-96 outline-none rounded border border-cyan p-2 text-gray1 text-sm lg:text-md'
 const textarea = 'bg-transparent text-gray1 hover:bg-bg2 outline-none rounded border-2 border-cyan p-2 h-40 lg:w-96 text-sm lg:text-md'
 const btn = 'px-4 py-2 rounded border-cyan lg:w-96 outline-none border-2 text-cyan font-semibold font-ls my-2 hover:bg-bg2 flex justify-center gap-3 items-center'
 const iconSend = 'hover:animate-spin delay-800 ease-out'
